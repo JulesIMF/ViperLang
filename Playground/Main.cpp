1c6a81e923cce119b0d1d54376cc592f77b693b7
JulesIMF <julesimf@yandex.ru>
2023-03-19 02:23:30 +0300

    [PLAYGROUND] Added playground

---
+++ Playground/Main.cpp
@@ -0,0 +1,32 @@
+/*++
+
+Copyright (c) 2023 JulesIMF, MIPT
+
+Module Name:
+
+    Main.cpp
+
+Abstract:
+
+    Main playground file.
+
+Author / Creation date:
+
+    JulesIMF / 19.03.23
+
+Revision History:
+
+--*/
+
+//
+// Includes / usings
+//
+
+//
+// Definitions
+//
+
+int
+main()
+{
+}
